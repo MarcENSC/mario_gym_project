3c96a9911cc00a6770d2ee7809622061313409c1
Marc M <mmoreeuw@ensc.fr>
2025-01-07 09:21:25 +0100

    malus going backwards

--- models/dqn_network/main.py
+++ models/dqn_network/main.py
@@ -28,8 +28,7 @@ env = ResizeObservation(env, shape=84)
 env = GrayScaleObservation(env, keep_dim=True)
 # 3. Skip 4 frames to speed up the training process :(84, 84, 1) -> (4, 84, 84, 1)
 env = FrameStack(env, num_stack=4)
-env = RecordVideo(env, video_folder="video/",episode_trigger=lambda x: x % 500 == 0 or x==2000 -1 or x==0, name_prefix="rl-video")
-
+env = RecordVideo(env, video_folder="video/", episode_trigger=lambda x: True, name_prefix="rl-video")
 
 env.reset()
 env.seed(0)
@@ -52,7 +51,7 @@ DQN = DQN()
 
 # DQN config
 config = {'nb_actions': env.action_space.n,
-          'train_warmup': 10000,
+          'train_warmup': 100,
           'train_freq': 10,
           'gradient_steps': 1,
           'learning_rate': 0.00025,
